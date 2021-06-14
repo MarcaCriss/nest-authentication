3c73f1f2ebc76d75bef084e229a928e531ff2d15
MarcaCriss <54193589+MarcaCriss@users.noreply.github.com>
2021-06-14 08:24:00 -0400

    login completo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +10,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('auth/login')
+  @UseGuards(AuthGuard('local'))
+  async login(@Request() req) {
+    return req.user;
   }
 }
